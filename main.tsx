da86c04079d635895df95f4aed2fcdee36439fcc
mercurioa <alessandro.mercurio.external@ynap.com>
2025-10-15 14:12:03 +0200

    move main.tsx

--- main.tsx
+++ main.tsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client'
 // translations
 
 // App Container
-import App from './App'
+import App from './src/App'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
